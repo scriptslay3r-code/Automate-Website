9ac1b9e6b8dbdcf3be6408e317c867b96cdd4b6e
scriptslay3r-code <Jackrabbitaltima@gmail.com>
2022-03-27 14:26:40 -0500

    update generate-email.py

--- generate-email.py
+++ generate-email.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options)
 #https://www.randomlists.com/email-addresses?qty=12
 
 def generateEmails():
