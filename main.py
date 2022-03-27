79a631ac021e148910878444ff9c9498d084e71b
scriptslay3r-code <Jackrabbitaltima@gmail.com>
2022-03-27 14:22:49 -0500

    Create main.py
    Thai is my current main file. However, it causes an error on Limewire. I believe it is the Captcha

---
+++ main.py
@@ -0,0 +1,82 @@
+import time
+
+from selenium import webdriver
+
+from selenium.webdriver.chrome.options import Options
+
+from art import *
+
+welcome = text2art("Welcome")
+
+going = text2art("Going")
+
+headless = text2art("Headless!")
+
+opening = text2art("Opening")
+
+browser = text2art("Browser!")
+
+options = Options()
+
+options.add_argument('--headless')
+
+options.add_argument('--disable-gpu')
+
+url =("https://limewire.com/invite/668f67358badde5e6de7")
+
+#emailList = open(emailList.txt, 'r')
+
+answer = input("Headless? [Y] for Yes: ")
+
+if answer == 'y':
+
+  print(welcome)
+
+  time.sleep(.5)
+
+  print(going)
+
+  time.sleep(.5)
+
+  print(headless)
+
+  time.sleep(.5)
+
+  driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
+
+else:
+
+  driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+  print(welcome)
+
+  time.sleep(.5)
+
+  print(opening)
+
+  time.sleep(.5)
+
+  print(browser)
+
+  time.sleep(.5)
+
+with open("emailList.txt") as emailList:
+
+    for line in emailList:
+
+        #print(item)
+
+        
+
+        driver.get(url); time.sleep(3) # Let the user actually see something! 
+
+        email_box = driver.find_element_by_name('waitlist[email]')
+
+        email_box.send_keys(line)
+
+        email_box.submit() 
+
+        time.sleep(3) # Let the user actually see something!
+
+        driver.quit()
+
